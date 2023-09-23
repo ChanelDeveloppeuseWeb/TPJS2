0e412ad046d10f99a0b6a2358a7b3a0ea0f8234e
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-23 19:33:18 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,9 @@
 function yearsOldValid(){
     let age = document.getElementById("age").value
-
-switch (age) {
-    case  5:
-        result = "valide"
-        break;
-
-    default:
-        result = "l age est errore"
-        break;
-}
+try{
+    
+   
+}catch()
 document.querySelector("#result").innerHTML = result
 console.log(age)
 }
