c237844bf5e9f1d223d44ffee388f7c51135c319
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-23 17:36:06 +0100

    wip

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function calcul(){
+let numberTwo;
+let numberThree
+let numberOne = document.querySelector("#number1").value
+     somme = numberOne + numberTwo * numberThree
+     result = somme
+    document.getElementById("resultat").value = result
+}
