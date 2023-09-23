9ec8cc8b48beae89cfc9b099e8c4b75068e7aa3e
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-23 19:46:22 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
     let i
-    for( i=0; i <= 100; i++){
-        result  =+ i++
+    for( i= 0; i <= 99; i++){
+        result  =+ i+1
     document.getElementById("number").innerHTML += result + "<br>"
 
     }
