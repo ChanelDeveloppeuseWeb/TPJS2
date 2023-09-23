b8cdd466cc302ed2a0d37092653a0d9e912846e0
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-23 19:18:16 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
-let numberOne;
-let numberTwo;
-let somme
- numberOne = prompt("entrez le premier nombre:");
- numberTwo = alert("entrez le deuxieme nombre");
- somme = numberOne + numberTwo
- prompt(somme)
-document.getElementById("result").innerHTML =   `la somme est: ${somme}`
+ 
+ 
+let numberOne = prompt("entrez le premier nombre:");
+ let numberTwo = prompt("entrez le deuxieme nombre:");
+let sum = numberOne + numberTwo 
+
+ numberOne = document.getElementById("number1")
+ numberTwo = document.getElementById("number2")
+// document.write=`la somme est: ${sum}` 
+document.getElementById("result").innerHTML = sum
+console.log(sum);
