bca7fa83f2ecbd02b484b3a128de865ef5a0ffff
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-23 19:37:29 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 function calcul(){
 let numberTwo;
-let numberThree
-let numberOne = document.querySelector("#number1").value
+let numberThree;
+let numberOne = document.querySelector("#number1")
      somme = numberOne + numberTwo * numberThree
      result = somme
     document.getElementById("resultat").value = result
